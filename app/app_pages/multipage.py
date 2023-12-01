15e9cf2fd0521ca656a1701e806c070eac699d38
cefeidas <>
2023-12-01 01:56:23 +0000

    Fix Trailing Whitespace and Newline Issues in multipage.py

--- app/app_pages/multipage.py
+++ app/app_pages/multipage.py
@@ -1,9 +1,4 @@
 import streamlit as st
-from app_pages import home_page
-from app_pages import project_introduction
-from app_pages import model_interaction
-from app_pages import results_conclusions
-
 
 
 class MultiPage:
@@ -15,7 +10,6 @@ class MultiPage:
         self.pages = []
         self.app_name = app_name
 
-
     def add_page(self, title, func) -> None:
         """
         Add a new page to the app.
@@ -25,25 +19,9 @@ class MultiPage:
         self.pages.append({"title": title, "function": func})
 
     def run(self):
-        """
-        Run the app and display the pages in the sidebar menu.
-        """
         st.title(self.app_name)
-        # Creating a sidebar for navigation
         page = st.sidebar.radio('Navigation', self.pages,
                                 format_func=lambda page: page['title'])
 
         # Running the selected page function
         page['function']()
-
-
-app = MultiPage("Cherry Leaves Mildew Detection App")
-
-# Adding pages to the app
-app.add_page("Home", home_page.display_page)
-app.add_page("Project Introduction", project_introduction.display_page)
-app.add_page("Model Interaction", model_interaction.display_page)
-app.add_page("Results and Conclusions", results_conclusions.display_page)
-
-if __name__ == "__main__":
-    app.run()
--- app/app_pages/project_introduction.py
+++ app/app_pages/project_introduction.py
@@ -4,4 +4,4 @@ import streamlit as st
 def display_page():
     st.title("Project Introduction")
     st.write(
-        "This page provides an overview of the cherry leaves mildew detection project.")
+        "This page introduces the cherry leaf mildew detection project.")
