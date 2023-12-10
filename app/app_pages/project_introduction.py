bc44e5d334e4deae0a3603a484a020f8368e3155
cefeidas <danmarmon1986@gmail.com>
2023-12-10 19:36:08 +0000

    adding our discoveries to the dashboard

--- app/app_pages/project_introduction.py
+++ app/app_pages/project_introduction.py
@@ -4,3 +4,18 @@ import streamlit as st
 def display_page(app):
     st.title("Project Introduction")
     st.write("This page outlines the project on cherry leaf mildew detection.")
+
+    st.info(
+    "**Project Overview**\n"
+    "* Objective: Identify mildew in cherry leaves using a CNN.\n"
+    "* Why: Mildew affects cherry crop health and yield.\n"
+    "* Approach: Use a CNN for autonomous pattern recognition.\n"
+    "* Data: Large dataset of images of cherry leaves.\n"
+    "* Classification: Healthy vs. Mildew-infected leaves.\n"
+    "* Note: CNN mimics human visual perception, no mathematical analysis of images.\n"
+    "* Hypothesis: Visible differences to humans should be detectable by CNN."
+)
+
+st.write(
+        f"* For more information and to understand the dataset better, "
+        f"please read the [Project Details](link_to_project_details).")
--- app/app_pages/results_conclusions.py
+++ app/app_pages/results_conclusions.py
@@ -4,3 +4,26 @@ import streamlit as st
 def display_page(app):
     st.title("Results and Conclusions")
     st.write("Explore outcomes and insights of the mildew project.")
+
+    # Button to evaluate the model
+    if st.button('Evaluate Model'):
+        model = load_model('cherry_leaf_cnn_model.h5')
+        evaluation_score = model.evaluate(test_generator)
+        accuracy = evaluation_score[1]como en
+        st.write(f"Model Accuracy: {accuracy:.2f}")
+
+    # Information about the model evaluation
+    st.info(
+        "**Model Evaluation**\n"
+        "* Model Accuracy: Approximately 98%.\n"
+        "* Industrial Benchmark: The industry standard for such tools is generally lower.\n"
+        "* Conclusion: Our model significantly surpasses reliability benchmarks, indicating high confidence in its predictions."
+    )
+
+    # Information about the initial hypothesis and its validation
+    st.info(
+        "**Hypothesis Validation**\n"
+        "* Initial Hypothesis: Visible differences between healthy and mildew-infected cherry leaves can be detected by a CNN.\n"
+        "* With the high accuracy achieved, we consider the initial hypothesis validated.\n"
+        "* Our CNN model successfully differentiates between the two categories, mimicking human visual perception."
+)
