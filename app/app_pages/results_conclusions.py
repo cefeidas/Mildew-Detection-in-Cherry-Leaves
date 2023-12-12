8bb80094b52c6b9d38ef47541952f70d6b2f6bcd
cefeidas <danmarmon1986@gmail.com>
2023-12-12 10:04:15 +0000

    delete the evaluate button from app, final users are uninterested

--- app/app_pages/results_conclusions.py
+++ app/app_pages/results_conclusions.py
@@ -2,34 +2,9 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Define the path to the test directory
-test_dir = 'inputs/mildew_dataset/cherry-leaves/test'
-
-# Image parameters
-img_width, img_height = 50, 50
-batch_size = 20
-
-# Create an ImageDataGenerator for image normalization
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-# Create the test_generator
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary'
-)
-
 def display_page(app):
     st.title("Results and Conclusions")
     st.write("Explore outcomes and insights of the mildew project.")
-
-    # Button to evaluate the model
-    if st.button('Evaluate Model'):
-        model = load_model('cherry_leaf_cnn_model.h5')
-        evaluation_score = model.evaluate(test_generator)
-        accuracy = evaluation_score[1]
-        st.write(f"Model Accuracy: {accuracy:.2f}")
 
 
     # Information about the model evaluation
